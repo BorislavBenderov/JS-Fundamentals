4e9727bd9867c0c59526f38156372e7ff21a881d
BorislavBenderov <benderow94@gmail.com>
2022-07-08 08:04:24 +0300

    Added new js files.

---
+++ Arrays Advanced/distinctArray.js
@@ -0,0 +1,12 @@
+function distinctArray(arr) {
+
+    let resultArray = [];
+
+    for (let i = 0; i < arr.length; i++) {
+
+        if (!resultArray.includes(arr[i])) {
+            resultArray.push(arr[i]);
+        }
+    }
+    console.log(resultArray);
+}
