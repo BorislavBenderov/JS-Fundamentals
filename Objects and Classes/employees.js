c3f0bc5696817f985c4846fa59262089f3b35c9a
BorislavBenderov <benderow94@gmail.com>
2022-07-20 19:29:53 +0300

    Added new js files.

---
+++ Objects and Classes/employees.js
@@ -0,0 +1,10 @@
+function employees(input) {
+    let listObject = {};
+
+    for (let person of input) {
+        listObject.name = person;
+        listObject.number = person.length;
+        console.log(`Name: ${listObject.name} -- Personal Number: ${listObject.number}`);
+    }
+
+}
