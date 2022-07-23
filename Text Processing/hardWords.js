a82779a511271f310b860c1186433821911f423d
BorislavBenderov <benderow94@gmail.com>
2022-07-23 22:00:22 +0300

    Added new js files.

---
+++ Text Processing/hardWords.js
@@ -0,0 +1,21 @@
+function hardWords(input) {
+
+    let text = input[0];
+    let missingWords = input[1];
+    let underscores = [];
+
+    for (let el of missingWords) {
+        let underscore = '_';
+        underscore = underscore.repeat(el.length);
+        underscores.push(underscore);
+    }
+
+    missingWords.sort((a, b) => b.length - a.length);
+    underscores.sort((a, b) => b.length - a.length);
+
+    for (let i = 0; i < underscores.length; i++) {
+        text = text.replace(underscores[i], missingWords[i]);
+    }
+
+    console.log(text);
+}
