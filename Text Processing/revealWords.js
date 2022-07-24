f204d719571648b3e9a7fb3a8565d6012af2c6fc
BorislavBenderov <benderow94@gmail.com>
2022-07-24 08:55:42 +0300

    Added new js files.

---
+++ Text Processing/revealWords.js
@@ -0,0 +1,15 @@
+function revealWords(words, sentence) {
+    let wordsToReveal = words.split(', ');
+    let sentenceArr = sentence.split(' ');
+
+    for (let element of wordsToReveal) {
+
+        for (let word of sentenceArr) {
+            if (word.includes('*') && word.length === element.length) {
+                sentence = sentence.replace(word, element);
+            }
+        }
+    }
+
+    console.log(sentence);
+}
